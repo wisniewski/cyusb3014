681ddd2f0a6ee32caf0eefe5a8ccf7c29c1be2dd
Mariusz Wisniewski <poczta@mariuszwisniewski.net>
2014-12-03 23:03:32 +0100

    LCD test with slide buttons

--- ISE Spartan 3E/SlaveFIFOMain_original.vhd
+++ ISE Spartan 3E/SlaveFIFOMain_original.vhd
@@ -13,20 +13,41 @@ use UNISIM.vcomponents.all;
 entity slave_fifo_main is 
 generic 
 (
-    fifo_mode_bit   : natural := 3
+    addr_bit    : natural := 2;         --how many bits
+    data_bit    : natural := 16;
+    pmode_bit   : natural := 3;
+    led_bit     : natural := 3
 );
 Port 
 (
     clock50     : in std_logic;         -- default input clock 50 MHz
-    pclk : out std_logic;
-    fifo_mode   : in std_logic_vector(fifo_mode_bit-1 downto 0):="000";             -- select mode: (000) idle, (001) loop, (010) stream out, (100) stream in
+    pclk        : out std_logic;        -- PCLK 100 MHz output to FX3
+
+    slcs        : out std_logic;        -- chip select
+    slwr        : out std_logic;        -- write strobe
+    slrd        : out std_logic;        -- read strobe
+    sloe        : out std_logic;        -- output enable
+    pktend      : out std_logic;
+
+    flaga       : in std_logic;         -- write
+    flagb       : in std_logic;         -- write
+    flagc       : in std_logic;         -- read
+    flagd       : in std_logic;         -- read
+
+    address     : out std_logic_vector(addr_bit-1 downto 0):="00";                      -- 2-bit address bus 
+    data      : inout std_logic_vector(data_bit-1 downto 0):="0000000000000000";      -- 16-bit data bus
+
+    pmode       : in std_logic_vector(pmode_bit-1 downto 0):="000";             -- select mode: (000) idle, (001) loop, (010) stream out, (100) stream in
     reset_n_con : in std_logic;                                                 -- input reset FIFO
-    
+    led_state   : out std_logic_vector(led_bit-1 downto 0):="111";              -- show current FSM state on LEDs (7, 5, 3)
+
+---------------------------------------------------------------------------------------------------------- lcd
     lcd_strataflash : out std_logic;
     lcd_e : out std_logic;
-    lcd_rs : out std_logic; -- 1 lcd_data register; 0 command/instruction
+    lcd_rs : out std_logic; -- 1 data register; 0 command/instruction
     lcd_rw : out std_logic;
-    lcd_data_out : out std_logic_vector(3 downto 0)
+    lcd_data : out std_logic_vector(3 downto 0)
+----------------------------------------------------------------------------------------------------------/lcd
 );
 end slave_fifo_main;
 ----------------------------------------------------------------------------------
@@ -38,9 +59,12 @@ architecture slave_fifo_main_arch of slave_fifo_main is
 ----------------------------------------------------------------------------------
 type fpga_master_states is (idle_state, loopback_state, stream_out_state, stream_in_state);
 signal current_state, next_state : fpga_master_states;
-
-type lcd_states is (start, clearscr, move1, move2, idle, send1, send2);
-signal lcd_current_state : lcd_states := start;
+---------------------------------------------------------------------------------------------------------- lcd
+signal data, goto : std_logic_vector(7 downto 0);
+signal data_request, clear, goto_request, request_served, display_ready : std_logic;
+type send_fsm is (start, clearscr, move1, move2, idle, send1, send2);
+signal fsm : send_fsm := start;
+----------------------------------------------------------------------------------------------------------/lcd
 ----------------------------------------------------------------------------------
 -- Constants
 ----------------------------------------------------------------------------------
@@ -56,14 +80,6 @@ signal reset_fpga : std_logic:='0';
 signal locked : std_logic:='0';
 signal clock100 : std_logic;
 signal current_mode : std_logic_vector(2 downto 0);
-
-signal lcd_data_in, goto : std_logic_vector(7 downto 0);
-signal data_request, clear, goto_request, request_served, display_ready : std_logic;
-----------------------------------------------------------------------------------
--- Text Variables
-----------------------------------------------------------------------------------
-signal text_line1 : string(1 to 12) := "Current Mode";
-signal text_line2 : string(1 to 16);
 ----------------------------------------------------------------------------------
 -- Components
 ----------------------------------------------------------------------------------
@@ -82,10 +98,10 @@ component lcd_controller port
     clock50 : in std_logic;
     reset_fpga : in std_logic;
     lcd_e : out std_logic;
-    lcd_rs : out std_logic;
+    lcd_rs : out std_logic; -- 1 data register; 0 command/instruction
     lcd_rw : out std_logic;
-    lcd_data_out : out std_logic_vector(3 downto 0);
-    lcd_data_input : in std_logic_vector(7 downto 0);
+    lcd_data_out : out std_logic_vector(data_out_bit-1 downto 0);
+    lcd_data_input : in std_logic_vector(data_in_bit-1 downto 0);
     lcd_data_request : in std_logic;
     lcd_clear : in std_logic;
     lcd_goto : in std_logic_vector(7 downto 0);
@@ -98,9 +114,7 @@ end component;
 -- Main code begin
 ----------------------------------------------------------------------------------
 begin
-----------------------------------------------------------------------------------
--- Generate 100 MHz Clock (DCM)
-----------------------------------------------------------------------------------
+-- -- -- -- -- -- Generate 100 MHz Clock (DCM)
 Inst_clock_dcm : clock_dcm PORT MAP
 (
     CLKIN_IN => clock50,
@@ -110,9 +124,10 @@ Inst_clock_dcm : clock_dcm PORT MAP
     CLK2X_OUT => clock100,
     LOCKED_OUT => locked
 );
-----------------------------------------------------------------------------------
--- LCD Controller Port Map
-----------------------------------------------------------------------------------
+--lcd
+---------------------------------------------------------------------------------------------------------- lcd
+strataflash_disable <= '1';
+
 lcd : lcd_controller port map
 (
     clock50 => clock50,
@@ -120,105 +135,38 @@ lcd : lcd_controller port map
     lcd_e => lcd_e,
     lcd_rs => lcd_rs,
     lcd_rw => lcd_rw,
-    lcd_data_out => lcd_data_out,
-    lcd_data_input => lcd_data_in,
-    lcd_data_request => data_request,
-    lcd_clear => clear,
-    lcd_goto => goto,
-    lcd_goto_request => goto_request,
-    lcd_request_served => request_served,
-    lcd_ready => display_ready
+    lcd_data_out => 
+    lcd_data_input => 
+    lcd_data_request => 
+    lcd_clear => 
+    lcd_goto => 
+    lcd_goto_request => 
+    lcd_request_served => 
+    lcd_ready => 
 );
--- -- -- -- -- -- Signals
-pclk <= clock100;               -- send to FX3 synch clock
 
-reset_dcm <= not reset_n_con;   -- original active low, so set it to high
-reset_fpga <= not locked;       -- if clock is not stable or DCM is not working
-
-lcd_strataflash <= '1';
-----------------------------------------------------------------------------------
--- FPGA Master-Mode Change State and Select Mode from Slide Switches
-----------------------------------------------------------------------------------
-process (clock100, reset_fpga) begin
-    if(reset_fpga='0') then
-        current_state <= idle_state;
-        current_mode <= MASTER_IDLE;
-    elsif (rising_edge(clock100)) then
-        current_state <= next_state;
-        current_mode <= fifo_mode;
-    end if;
-end process;
-----------------------------------------------------------------------------------
--- FPGA State Machine
-----------------------------------------------------------------------------------
-process(current_state, current_mode) begin
-    case current_state is
-        when idle_state =>
-            case current_mode is
-                when LOOPBACK =>
-                    next_state <= loopback_state;
-                when STREAM_OUT =>
-                    next_state <= stream_out_state;
-                when STREAM_IN =>
-                    next_state <= stream_in_state;
-                when others =>
-                    next_state <= idle_state;
-            end case;
-
-        when loopback_state =>
-            case current_mode is
-                when LOOPBACK =>
-                    next_state <= loopback_state;
-                    text_line2 <= "Loopback Mode   ";
-                when others =>
-                    next_state <= idle_state; 
-            end case;
-
-        when stream_out_state =>
-            case current_mode is
-                when STREAM_OUT =>
-                    next_state <= stream_out_state;
-                    text_line2 <= "Stream Out Mode ";
-                when others =>
-                    next_state <= idle_state;
-            end case;
-
-        when stream_in_state =>
-            case current_mode is
-                when STREAM_IN =>
-                    next_state <= stream_in_state;
-                    text_line2 <= "Stream In Mode  ";
-                when others =>
-                    next_state <= idle_state;
-            end case;
-
-        when others => 
-            next_state <= idle_state;
-    end case;
-end process;
-----------------------------------------------------------------------------------
--- LCD Main Process
-----------------------------------------------------------------------------------
-lcd_show : process(reset_fpga, clock50, request_served)
+test : process(reset_fpga, clock50, request_served)
         variable counter : integer range 0 to 50000000 := 1;
+        constant line1 : string(1 to 11) := "Hola mundo.";
+        constant line2 : string(1 to 7) := "World 4";
         variable letter_index : integer := 0;
     begin
         if reset_fpga = '1' then
             counter := 1;
-            lcd_current_state <= start;
+            fsm <= start;
         elsif rising_edge(clock50) then
-            case lcd_current_state is
+            case fsm is
                 when start =>
                     if display_ready = '1' then
                         counter := 0;
-                        lcd_current_state <= move1;
+                        fsm <= move1;
                     end if;
                 when clearscr =>
                     if counter = 1 then
                         clear <= '1';
                     elsif request_served = '1' then
                         clear <= '0';
-                        lcd_current_state <= move1;
+                        fsm <= move1;
                         counter := 0;
                     end if;
                 when move1 =>
@@ -227,19 +175,19 @@ lcd_show : process(reset_fpga, clock50, request_served)
                         goto_request <= '1';
                     elsif request_served = '1' then
                         goto_request <= '0';
-                        lcd_current_state <= send1;
+                        fsm <= send1;
                         counter := 0;
                     end if;
                 when send1 =>
                     if counter = 1 then
                         letter_index := letter_index + 1;
-                        lcd_data_in <= conv_std_logic_vector(character'pos(text_line1(letter_index)), 8);
+                        data <= conv_std_logic_vector(character'pos(line1(letter_index)), 8);
                         data_request <= '1';
                     elsif request_served = '1' then
                         data_request <= '0';
                         counter := 0;
-                        if letter_index = text_line1'length then
-                            lcd_current_state <= move2;
+                        if letter_index = line1'length then
+                            fsm <= move2;
                         end if;
                     end if;
                 when move2 =>
@@ -248,20 +196,20 @@ lcd_show : process(reset_fpga, clock50, request_served)
                         goto_request <= '1';
                     elsif request_served = '1' then
                         goto_request <= '0';
-                        lcd_current_state <= send2;
+                        fsm <= send2;
                         counter := 0;
                         letter_index := 0;
                     end if;
                 when send2 =>
                     if counter = 1 then
                         letter_index := letter_index + 1;
-                        lcd_data_in <= conv_std_logic_vector(character'pos(text_line2(letter_index)), 8);
+                        data <= conv_std_logic_vector(character'pos(line2(letter_index)), 8);
                         data_request <= '1';
                     elsif request_served = '1' then
                         data_request <= '0';
                         counter := 0;
-                        if letter_index = text_line2'length then
-                            lcd_current_state <= idle;
+                        if letter_index = line2'length then
+                            fsm <= idle;
                         end if;
                     end if;
                 when others => null;
@@ -269,6 +217,57 @@ lcd_show : process(reset_fpga, clock50, request_served)
             counter := counter + 1;
         end if;
     end process;
+
+
+----------------------------------------------------------------------------------------------------------/lcd
+
+-- -- -- -- -- -- Signals
+led_state <= pmode;
+address <= "00";
+slcs <= '0';
+sloe <= '0';
+slrd <= '0';
+pktend <= '1';
+slwr <= '0';
+pclk <= clock100;               -- send to FX3 synch clock
+
+reset_dcm <= not reset_n_con;   -- original active low, so set it to high
+reset_fpga <= not locked;       -- if clock is not stable or DCM is not working
+-- -- -- -- -- -- FPGA Master-Mode Change State
+process (clock100, reset_fpga) begin
+    if(reset_fpga='0') then
+        current_state <= idle_state;
+    elsif (rising_edge(clock100)) then
+        current_state <= next_state;
+    end if;
+end process;
+-- -- -- -- -- -- Select mode from board switches
+process(clock100, reset_fpga) begin
+    if reset_fpga='0' then
+        current_mode <= MASTER_IDLE;
+    elsif(rising_edge(clock100)) then
+        current_mode <= pmode;
+    end if;
+end process;
+-- -- -- -- -- FPGA State Machine
+--process(current_state, current_mode) begin
+--  case current_state is
+--      when idle_state =>
+--          case current_mode is
+--              when LOOPBACK =>
+--                  next_state <= loopback_state;
+--              when STREAM_OUT =>
+--                  next_state <= stream_out_state;
+--              when STREAM_IN =>
+--                  next_state <= stream_in_state;
+--              when others =>
+--                  next_state <= idle_state;
+--          end case;
+--      when loopback_state =>
+--
+--
+--  end case;
+--end process;
 ----------------------------------------------------------------------------------
 -- End Architecture
 ----------------------------------------------------------------------------------
--- ISE Spartan 3E/lcd_controller.vhd
+++ ISE Spartan 3E/lcd_controller.vhd
@@ -1,121 +1,139 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    19:59:30 06/19/2012 
--- Design Name: 
--- Module Name:    HitachiLCDController - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- LCD Controller Spartan 3E
 ----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity HitachiLCDController is
-port(
-  mclock : in std_logic;
-  reset : in std_logic;
-  enabled : out std_logic;
-  rs : out std_logic; -- 1 data register; 0 command/instruction
-  rw : out std_logic;
-  db : out std_logic_vector(3 downto 0);
-  data : in std_logic_vector(7 downto 0);
-  data_request : in std_logic;
-  clear : in std_logic;
-  goto : in std_logic_vector(7 downto 0);
-  goto_request : in std_logic;
-  request_served : out std_logic := '0';
-  display_ready : buffer std_logic := '0'
+library ieee;
+use ieee.std_logic_1164.all;
+----------------------------------------------------------------------------------
+-- Entity
+----------------------------------------------------------------------------------
+entity lcd_controller is
+generic 
+(
+  data_out_bit : natural := 4;
+  data_in_bit : natural := 8
 );
-end HitachiLCDController;
+port
+(
+  clock50 : in std_logic;
+  reset_fpga : in std_logic;
+  lcd_e : out std_logic;
+  lcd_rs : out std_logic; -- 1 data register; 0 command/instruction
+  lcd_rw : out std_logic;
+  lcd_data_out : out std_logic_vector(data_out_bit-1 downto 0);
+  lcd_data_input : in std_logic_vector(data_in_bit-1 downto 0);
+  lcd_data_request : in std_logic;
+  lcd_clear : in std_logic;
+  lcd_goto : in std_logic_vector(7 downto 0);
+  lcd_goto_request : in std_logic;
+  lcd_request_served : out std_logic := '0';
+  lcd_ready : buffer std_logic := '0'
+);
+end lcd_controller;
+----------------------------------------------------------------------------------
+-- Architecture
+----------------------------------------------------------------------------------
+architecture lcd_controller_arch of lcd_controller is
+----------------------------------------------------------------------------------
+-- LCD Controller Finished State Machine
+----------------------------------------------------------------------------------  
+type init_state is 
+(
+  waiting, 
+  send_three, 
+  wait_after_three, 
+  conf_four_bits, 
+  wait_after_bits
+);
+type conf_state is 
+(
+  function_set, 
+  set_display, 
+  display_clear, 
+  entry_mode_set, 
+  finish
+);
+type send_byte is 
+(
+  setup_high_nibble, 
+  send_high_nibble, 
+  nibble_separation, 
+  setup_low_nibble, 
+  send_low_nibble, 
+  finish, 
+  idle
+);
+type write_state is 
+(
+  idle, 
+  clearing, 
+  moving, 
+  writing, 
+  waiting
+);
+----------------------------------------------------------------------------------
+-- Signals
+----------------------------------------------------------------------------------
+signal init_fsm : init_state := waiting;
+signal config_fsm : conf_state := function_set;
+signal send_byte_fsm : send_byte := idle;
+signal write_fsm : write_state := idle;
 
-architecture Behavioral of HitachiLCDController is
-  
-  type init_state is (waiting, send_three, wait_after_three, conf_four_bits, wait_after_bits);
-  type conf_state is (function_set, set_display, display_clear, entry_mode_set, finish);
-  type send_byte is (setup_high_nibble, send_high_nibble, nibble_separation, setup_low_nibble, 
-    send_low_nibble, finish, idle);
-    
-  type write_state is (idle, clearing, moving, writing, waiting);
-  
-  signal write_fsm : write_state := idle;
-  
-  signal init_fsm : init_state := waiting;
-  signal init_nibble : std_logic;
-  signal init_enabled : std_logic;
-  signal configuring : std_logic := '1';
-  
-  signal config_fsm : conf_state := function_set;
-  
-  signal data_nibble : std_logic_vector(3 downto 0);
-  signal data_enabled : std_logic;
-  signal config_register_select : std_logic := '0';
-  signal register_select : std_logic := '0';
-  
-  signal curr_byte : std_logic_vector(7 downto 0);
-  signal byte_to_send : std_logic_vector(7 downto 0);
-  signal config_byte_to_send : std_logic_vector(7 downto 0);
-  
-  signal send_byte_request : std_logic := '0';
-  signal byte_sent : std_logic := '0';
-  signal send_byte_fsm : send_byte := idle;
-  
-  signal send_config_byte_request : std_logic := '0';
-  signal send_request : std_logic := '0';
-  
+signal init_nibble : std_logic;
+signal init_enabled : std_logic;
+signal configuring : std_logic := '1';
+
+signal data_nibble : std_logic_vector(3 downto 0);
+signal data_enabled : std_logic;
+signal config_register_select : std_logic := '0';
+signal register_select : std_logic := '0';
+
+signal curr_byte : std_logic_vector(7 downto 0);
+signal byte_to_send : std_logic_vector(7 downto 0);
+signal config_byte_to_send : std_logic_vector(7 downto 0);
+
+signal send_byte_request : std_logic := '0';
+signal byte_sent : std_logic := '0';
+
+signal send_config_byte_request : std_logic := '0';
+signal send_request : std_logic := '0';
+----------------------------------------------------------------------------------
+-- Main code begin
+----------------------------------------------------------------------------------
 begin
 
-  rs <= config_register_select when display_ready = '0' else register_select;
-  rw <= '0';
+lcd_rs <= config_register_select when lcd_ready = '0' else register_select;
+lcd_rw <= '0';
+
+lcd_data_out <= ("001" & init_nibble) when configuring = '1' else data_nibble;
+lcd_e <= init_enabled when configuring = '1' else data_enabled;
+send_byte_request <= send_config_byte_request when lcd_ready = '0' else send_request;
+curr_byte <= config_byte_to_send when lcd_ready = '0' else byte_to_send;
   
-  db <= ("001" & init_nibble) when configuring = '1' else data_nibble;
-  enabled <= init_enabled when configuring = '1' else data_enabled;
-  send_byte_request <= send_config_byte_request when display_ready = '0' else send_request;
-  curr_byte <= config_byte_to_send when display_ready = '0' else byte_to_send;
-  
-  data_write : process(reset, mclock, data_request, goto_request, clear)
-    variable counter : integer range 0 to 3000 := 1;
+data_write : process(reset_fpga, clock50, lcd_data_request, lcd_goto_request, lcd_clear)
+  variable counter : integer range 0 to 3000 := 1;
   begin
-    if reset = '1' then
+    if reset_fpga = '1' then
       counter := 0;
-    elsif rising_edge(mclock) and display_ready = '1' then
-      request_served <= '0';
+    elsif rising_edge(clock50) and lcd_ready = '1' then
+      lcd_request_served <= '0';
       send_request <= '0';
       case write_fsm is
         when idle =>
           counter := 0;
-          if clear = '1' then
+          if lcd_clear = '1' then
             write_fsm <= clearing;
             register_select <= '0';
             byte_to_send <= "00000001";
             send_request <= '1';
-          elsif goto_request = '1' then
+          elsif lcd_goto_request = '1' then
             write_fsm <= moving;
             register_select <= '0';
-            byte_to_send <= goto;
+            byte_to_send <= lcd_goto;
             send_request <= '1';
-          elsif data_request = '1' then
+          elsif lcd_data_request = '1' then
             write_fsm <= writing;
             register_select <= '1';
-            byte_to_send <= data;
+            byte_to_send <= lcd_data_input;
             send_request <= '1';
           end if;
         when clearing =>
@@ -126,17 +144,17 @@ begin
         when waiting =>
           if counter > 82000 then
             write_fsm <= idle;
-            request_served <= '1';
+            lcd_request_served <= '1';
           end if;
         when moving =>
           if byte_sent = '1' then
             write_fsm <= idle;
-            request_served <= '1';
+            lcd_request_served <= '1';
           end if;
         when writing =>
           if byte_sent = '1' then
             write_fsm <= idle;
-            request_served <= '1';
+            lcd_request_served <= '1';
           end if;
       end case;
       
@@ -144,16 +162,16 @@ begin
     end if;
   end process;
   
-  init : process(reset, mclock)
-    variable counter : integer range 0 to 760000;
-    variable times_sent_number_three : integer range 0 to 3;
-    variable wait_time : integer range 2000 to 205000;
+init : process(reset_fpga, clock50)
+  variable counter : integer range 0 to 760000;
+  variable times_sent_number_three : integer range 0 to 3;
+  variable wait_time : integer range 2000 to 205000;
   begin
-    if reset = '1' then
+    if reset_fpga = '1' then
       configuring <= '1';
       counter := 0;
       init_fsm <= waiting;
-    elsif rising_edge(mclock) and configuring = '1' then
+    elsif rising_edge(clock50) and configuring = '1' then
       case init_fsm is
         when waiting => -- Wait after power on (15ms)
           init_enabled <= '0';
@@ -206,15 +224,15 @@ begin
     end if;
   end process;
   
-  configure_display : process(reset, mclock, configuring)
-    variable counter : integer range 0 to 83000 := 1; -- We still need to adjust this value
+configure_display : process(reset_fpga, clock50, configuring)
+  variable counter : integer range 0 to 83000 := 1; -- We still need to adjust this value
   begin
-    if reset = '1' then
-      display_ready <= '0';
+    if reset_fpga = '1' then
+      lcd_ready <= '0';
       counter := 1;
       config_fsm <= function_set;
       config_register_select <= '0';
-    elsif rising_edge(mclock) and configuring = '0' and display_ready = '0' then
+    elsif rising_edge(clock50) and configuring = '0' and lcd_ready = '0' then
       send_config_byte_request <= '0';
       config_register_select <= '0';
       case config_fsm is
@@ -249,7 +267,7 @@ begin
         when finish =>
           if counter > 82000 then
             counter := 0;
-            display_ready <= '1';
+            lcd_ready <= '1';
           end if;
       end case;
       
@@ -257,15 +275,15 @@ begin
     end if;
   end process;
   
-  send_bytes : process(reset, mclock, send_byte_request)
-    variable counter : integer range 0 to 2500 := 1;
-    variable data_tmp : std_logic_vector(7 downto 0);
+send_bytes : process(reset_fpga, clock50, send_byte_request)
+  variable counter : integer range 0 to 2500 := 1;
+  variable data_tmp : std_logic_vector(7 downto 0);
   begin
-    if reset = '1' then
+    if reset_fpga = '1' then
       send_byte_fsm <= idle;
       counter := 1;
       data_enabled <= '0';
-    elsif rising_edge(mclock) and configuring = '0' then
+    elsif rising_edge(clock50) and configuring = '0' then
       byte_sent <= '0';
       case send_byte_fsm is
         when idle =>
@@ -318,6 +336,7 @@ begin
       counter := counter + 1;
     end if;
   end process;
-  
-end Behavioral;
-
+----------------------------------------------------------------------------------
+-- End Architecture
+----------------------------------------------------------------------------------
+end lcd_controller_arch;
